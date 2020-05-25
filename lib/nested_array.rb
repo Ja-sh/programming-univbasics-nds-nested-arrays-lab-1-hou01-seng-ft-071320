39bec9c20b0f52a741c73388ff006f920acaeea0
Joshua Moore <morejoshua99@hotmail.com>
2020-05-25 17:24:24 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -18,7 +18,7 @@ CONVENTIONAL_PRODUCE = [
 
 def assembled_matrix
   array = [
-    ["Grapefruit",  "Pineapple",  "Oranges",  "Watermelon", "Eggplant"]
+    ["Grapefruit",  "Pineapple",  "Oranges",  "Watermelon", "Eggplant"],
     ["Strawberries",  "Potatoes", "Grapes", "Avocadoes",  "Asparagus"]
     
     ]
